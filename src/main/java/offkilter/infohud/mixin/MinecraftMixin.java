1b1f743e9084dbb19f7007b72fbfc618965f69a2
offkiltermc <offkilter@offkiltermc.net>
2022-07-17 09:33:06 -0700

    Add more info to FPS

--- src/main/java/offkilter/infohud/mixin/MinecraftMixin.java
+++ src/main/java/offkilter/infohud/mixin/MinecraftMixin.java
@@ -2,6 +2,7 @@ package offkilter.infohud.mixin;
 
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(Minecraft.class)
